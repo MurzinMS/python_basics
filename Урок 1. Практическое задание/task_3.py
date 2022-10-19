f5863be576029c42979d6a023dcb8ca0fc43255d
MurzinMS <Murzin.MS@gmail.com>
2022-10-19 15:57:33 +0300

    Export Lessons_1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,13 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+n = int(input("Введите число n: "))
+if n > 0:
+    nn = f"{n}{n}"
+    nnn = f"{nn}{n}"
+    result = n + int(nn) + int(nnn)
+    print(f"n + nn + nnn = {result}")
+else:
+>>>>>>> 36bedc5 (New_data)
+    print(f"Введенное число должно быть положительным")
