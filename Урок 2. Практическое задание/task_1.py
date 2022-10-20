dbd58b1a37c37a0cc5b8652a4728868152655c2d
MurzinMS <Murzin.MS@gmail.com>
2022-10-20 08:34:38 +0300

    Export lessons 2

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -15,16 +15,6 @@
 <class 'bool'>
 <class 'NoneType'>
 """
-any_types = ["abc", 34, True, 0.12367, None, [1, 2, 3], ('a', 'b', 'c'),
-             {11, 3, 8, 4}, {"key1": "val1", "key2": "val2"}, b'bytes']
-
-for el in any_types:
-   print(type(el))
-print("---------------------------------")
-
-for el in any_types:
-    print(f"element '{el}' of type {type(el)}")
-print("---------------------------------")
-
-list_of_string = list('simple string')
-print(list_of_string)
+lst_1 = [5, "string", 0.15, True, None]
+for el in lst_1:
+    print(type(el))
